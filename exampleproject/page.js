5f383cd257c81dc9c152febe6b3d7fd9dafcc7e0
Eduardo Gomes <163359240+eduardgomes@users.noreply.github.com>
2025-02-25 00:35:30 +0000

    add buttom

--- exampleproject/page.js
+++ exampleproject/page.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 function Texto1() {
     return <p>Um outro parágrafo em texto.</p>
 }
 
 export default function Home() {
+    const [hide, setHide] = useState(false);
     const aula = 2;
     return (
         <div>
     <h1>Olá, turma!!!</h1>
     <p>Essa é nossa {aula}ª de React</p>
+    <hr>
+    <button> { hide ? "Show" : ""}</button>
+    </hr>
+    {!hide && (
+        <>
     <Texto1 />
+    </>
+    )}
         </div>
     );
 }
